f8d3c96789ba64614a9657cb0884b9515fdf57a5
Marvin Ferber <ferbhome@googlemail.com>
2023-03-06 12:48:58 +0100

    added interaction on summand 1 using fire buttons of GameZip64

--- main.py
+++ main.py
@@ -1,55 +1,102 @@
-def singleadd(bit1: str, bit2: str, cin: str):
-    global bout, cout
-    if bit1 == "0" and bit2 == "0" and cin == "0":
-        bout = "0"
+def dez2bin(zahl22: number):
+    global ret, rest
+    ret = ""
+    while True:
+        # Division mit Rest, speichere nur den Rest
+        rest = zahl22 % 2
+        # Division mit Rest, speichere den ganzzahligen Quotient
+        zahl22 = Math.idiv(zahl22, 2)
+        # füge dem Ergebnis ein weiteres Bit rechts hinzu
+        ret = "" + str(rest) + ret
+        # Falls die Zahl 0 geworden ist, ist die Rechnung beendet
+        if zahl22 == 0:
+            break
+    while len(ret) < 8:
+        # füge dem Ergebnis ein weiteres Bit rechts hinzu
+        ret = "0" + ret
+    return ret
+def volladdierer(bitA: str, bitB: str, cin: str):
+    global sum2, cout
+    if bitA == "0" and bitB == "0" and cin == "0":
+        sum2 = "0"
         cout = "0"
-    if bit1 == "0" and bit2 == "0" and cin == "1":
-        bout = "1"
+    if bitA == "0" and bitB == "0" and cin == "1":
+        sum2 = "1"
         cout = "0"
-    if bit1 == "0" and bit2 == "1" and cin == "0":
-        bout = "1"
+    if bitA == "0" and bitB == "1" and cin == "0":
+        sum2 = "1"
         cout = "0"
-    if bit1 == "0" and bit2 == "1" and cin == "1":
-        bout = "0"
+    if bitA == "0" and bitB == "1" and cin == "1":
+        sum2 = "0"
         cout = "1"
-    if bit1 == "1" and bit2 == "0" and cin == "0":
-        bout = "1"
+    if bitA == "1" and bitB == "0" and cin == "0":
+        sum2 = "1"
         cout = "0"
-    if bit1 == "1" and bit2 == "0" and cin == "1":
-        bout = "0"
+    if bitA == "1" and bitB == "0" and cin == "1":
+        sum2 = "0"
         cout = "1"
-    if bit1 == "1" and bit2 == "1" and cin == "0":
-        bout = "0"
+    if bitA == "1" and bitB == "1" and cin == "0":
+        sum2 = "0"
         cout = "1"
-    if bit1 == "1" and bit2 == "1" and cin == "1":
-        bout = "1"
+    if bitA == "1" and bitB == "1" and cin == "1":
+        sum2 = "1"
         cout = "1"
+
+def my_function():
+    global summand1, input1, anzeige
+    summand1 += 1
+    input1 = dez2bin(summand1)
+    basic.show_string(input1)
+    anzeige = addition(input1, input2)
+GAME_ZIP64.on_button_press(GAME_ZIP64.ZIP64ButtonPins.FIRE1,
+    GAME_ZIP64.ZIP64ButtonEvents.CLICK,
+    my_function)
+
 def addition(bitstr1: str, bitstr2: str):
-    global ergebnis, cout
+    global ergebnis, cout, Index
     ergebnis = ""
     cout = "0"
-    Index = 0
     while Index <= len(bitstr1) - 1:
-        singleadd(bitstr1.char_at(len(bitstr1) - 1-Index), bitstr2.char_at(len(bitstr1) - 1-Index), cout)
-        ergebnis = "" + bout + ergebnis
+        volladdierer(bitstr1.char_at(len(bitstr1) - 1 - Index),
+            bitstr2.char_at(len(bitstr1) - 1 - Index),
+            cout)
+        ergebnis = "" + sum2 + ergebnis
         Index += 1
     return ergebnis
 Index2 = 0
+Index = 0
 ergebnis = ""
 cout = ""
-bout = ""
-input1 = "001"
-input2 = "001"
+sum2 = ""
+zahl222 = 0
+rest = 0
+ret = ""
+anzeige = ""
+summand1 = 3
+zahl2222 = 0
+display = GAME_ZIP64.create_zip64_display()
+display.set_brightness(20)
+input1 = dez2bin(summand1)
+input2 = "00000111"
 anzeige = addition(input1, input2)
 
 def on_forever():
-    global Index2
+    global Index2, anzeige, input1, input2
     while Index2 <= len(input1):
         if input1.char_at(Index2) == "1":
-            led.plot(Index2, 0)
+            display.set_matrix_color(Index2, 0, GAME_ZIP64.colors(ZipLedColors.RED))
+        else:
+            display.set_matrix_color(Index2, 0, GAME_ZIP64.colors(ZipLedColors.WHITE))
         if input2.char_at(Index2) == "1":
-            led.plot(Index2, 1)
+            display.set_matrix_color(Index2, 1, GAME_ZIP64.colors(ZipLedColors.RED))
+        else:
+            display.set_matrix_color(Index2, 1, GAME_ZIP64.colors(ZipLedColors.WHITE))
+        display.set_matrix_color(Index2, 2, GAME_ZIP64.colors(ZipLedColors.GREEN))
         if anzeige.char_at(Index2) == "1":
-            led.plot(Index2, 3)
+            display.set_matrix_color(Index2, 3, GAME_ZIP64.colors(ZipLedColors.RED))
+        else:
+            display.set_matrix_color(Index2, 3, GAME_ZIP64.colors(ZipLedColors.WHITE))
         Index2 += 1
+    display.show()
+    basic.pause(200)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,58 +1,100 @@
+function dez2bin (zahl22: number) {
+    ret = ""
+    while (true) {
+        // Division mit Rest, speichere nur den Rest
+        rest = zahl22 % 2
+        // Division mit Rest, speichere den ganzzahligen Quotient
+        zahl22 = Math.idiv(zahl22, 2)
+        // füge dem Ergebnis ein weiteres Bit rechts hinzu
+        ret = "" + rest + ret
+        // Falls die Zahl 0 geworden ist, ist die Rechnung beendet
+        if (zahl22 == 0) {
+            break;
+        }
+    }
+    while (ret.length < 8) {
+        // füge dem Ergebnis ein weiteres Bit rechts hinzu
+        ret = "0" + ret
+    }
+    return ret
+}
 function volladdierer (bitA: string, bitB: string, cin: string) {
     if (bitA == "0" && bitB == "0" && cin == "0") {
-        sum = "0"
+        sum2 = "0"
         cout = "0"
     }
     if (bitA == "0" && bitB == "0" && cin == "1") {
-        sum = "1"
+        sum2 = "1"
         cout = "0"
     }
     if (bitA == "0" && bitB == "1" && cin == "0") {
-        sum = "1"
+        sum2 = "1"
         cout = "0"
     }
     if (bitA == "0" && bitB == "1" && cin == "1") {
-        sum = "0"
+        sum2 = "0"
         cout = "1"
     }
     if (bitA == "1" && bitB == "0" && cin == "0") {
-        sum = "1"
+        sum2 = "1"
         cout = "0"
     }
     if (bitA == "1" && bitB == "0" && cin == "1") {
-        sum = "0"
+        sum2 = "0"
         cout = "1"
     }
     if (bitA == "1" && bitB == "1" && cin == "0") {
-        sum = "0"
+        sum2 = "0"
         cout = "1"
     }
     if (bitA == "1" && bitB == "1" && cin == "1") {
-        sum = "1"
+        sum2 = "1"
         cout = "1"
     }
 }
+GAME_ZIP64.onButtonPress(GAME_ZIP64.ZIP64ButtonPins.Fire1, GAME_ZIP64.ZIP64ButtonEvents.Click, function () {
+    summand1 += 1
+    input1 = dez2bin(summand1)
+    anzeige = addition(input1, input2)
+})
 function addition (bitstr1: string, bitstr2: string) {
     ergebnis = ""
     cout = "0"
+    Index = 0
     while (Index <= bitstr1.length - 1) {
         volladdierer(bitstr1.charAt(bitstr1.length - 1 - Index), bitstr2.charAt(bitstr1.length - 1 - Index), cout)
-        ergebnis = "" + sum + ergebnis
+        ergebnis = "" + sum2 + ergebnis
         Index += 1
     }
     return ergebnis
 }
+GAME_ZIP64.onButtonPress(GAME_ZIP64.ZIP64ButtonPins.Fire2, GAME_ZIP64.ZIP64ButtonEvents.Click, function () {
+    summand1 += -1
+    input1 = dez2bin(summand1)
+    anzeige = addition(input1, input2)
+})
 let Index2 = 0
 let Index = 0
 let ergebnis = ""
 let cout = ""
-let sum = ""
+let sum2 = ""
+let zahl22 = 0
+let rest = 0
+let ret = ""
+let anzeige = ""
+let input2 = ""
+let input1 = ""
+let summand1 = 0
+let zahl2222 = 0
+let zahl222 = 0
+summand1 = 3
 let display = GAME_ZIP64.createZIP64Display()
 display.setBrightness(20)
-let input1 = "00000011"
-let input2 = "00000111"
-let anzeige = addition(input1, input2)
+input1 = dez2bin(summand1)
+input2 = "00000111"
+anzeige = addition(input1, input2)
 basic.forever(function () {
+    Index2 = 0
     while (Index2 <= input1.length) {
         if (input1.charAt(Index2) == "1") {
             display.setMatrixColor(Index2, 0, GAME_ZIP64.colors(ZipLedColors.Red))
@@ -73,4 +115,5 @@ basic.forever(function () {
         Index2 += 1
     }
     display.show()
+    basic.pause(200)
 })
